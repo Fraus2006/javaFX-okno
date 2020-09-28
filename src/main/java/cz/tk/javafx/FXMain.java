7675b73dee75f7a512091a5603bf67fd9514a07c
Tomas Kraus <t.kraus@post.cz>
2020-09-28 17:56:47 +0200

    Initial project.

---
+++ src/main/java/cz/tk/javafx/FXMain.java
@@ -0,0 +1,17 @@
+/*
+ * (C) 2020, Tomas Kraus
+ */
+
+package cz.tk.javafx;
+
+/**
+ * Programm startup class.
+ */
+public class FXMain {
+
+    public static final void main(String[] args) {
+        System.out.println("Necum xichte, smrdis!");
+    }
+
+
+}
