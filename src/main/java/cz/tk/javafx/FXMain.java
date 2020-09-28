3a326b7dad47f82e89a34b96ef534765b1f36e44
Tomas Kraus <t.kraus@post.cz>
2020-09-28 18:19:51 +0200

    First working window.

--- src/main/java/cz/tk/javafx/FXMain.java
+++ src/main/java/cz/tk/javafx/FXMain.java
@@ -4,13 +4,44 @@
 
 package cz.tk.javafx;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 /**
  * Programm startup class.
  */
-public class FXMain {
+public class FXMain extends Application {
+
+    private static class HelloWorldEventHandler implements EventHandler<ActionEvent> {
+        @Override
+        public void handle(ActionEvent event) {
+            System.out.println("Hello World!");
+        }
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new HelloWorldEventHandler());
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     public static final void main(String[] args) {
-        System.out.println("Necum xichte, smrdis!");
+        launch(args);
     }
 
 
